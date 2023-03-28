8ba54836f7c43e4c0967af878e3192d259051ae1
Aldi-H <aldihimawan88@gmail.com>
2023-03-28 14:29:58 +0700

    chore: update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,96 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <Wire.h>
-#include "GravityTDS.h"
-
-GravityTDS gravityTds;
 
 #define TdsSensorPin 34
+#define VREF 3.3  // analog reference voltage (V) ADC
+#define SCOUNT 20 // sum of sample point
 
-float temperature = 25.0, tdsValue = 0.0;
+int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
 
-// potentiometer connected to GPIO34 (ADC1_CH6)
-const int potPin = 34;
+float averageVoltage = 0;
+float tdsValue = 0;
 
-// variable to store the value read from the potentiometer
-int potValue = 0;
+// Change this using temperature water sensor
+float temperature = 25; // current temperature for compensation
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  gravityTds.setPin(TdsSensorPin);
-  gravityTds.setAref(3.0);        // reference voltage on ADC
-  gravityTds.setAdcRange(4095.0); // 1024 for 10bit ADC; 4095 for 12bit ADC
-  gravityTds.begin();
+// median filtering algorithm
+int getMedianNum(int bArray[], int iFilterLen)
+{
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++)
+  {
+    for (i = 0; i < iFilterLen - j - 1; i++)
+    {
+      if (bTab[i] > bTab[i + 1])
+      {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0)
+  {
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  }
+  else
+  {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  gravityTds.setTemperature(temperature);
-  gravityTds.update();
-  tdsValue = gravityTds.getTdsValue();
-  potValue = analogRead(potPin);
-  Serial.print("ADC Value: ");
-  Serial.print(potValue);
-  Serial.printf("    ");
-  Serial.print("TDS Value: ");
-  Serial.print(tdsValue);
-  Serial.print("ppm");
-  delay(4000);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(TdsSensorPin, INPUT);
+}
+
+void loop()
+{
+  static unsigned long analogSampleTimepoint = millis();
+  if (millis() - analogSampleTimepoint > 50U)
+  { // every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin); // read the analog value and store into the buffer
+
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT)
+    {
+      analogBufferIndex = 0;
+    }
+  }
+
+  static unsigned long printTimepoint = millis();
+  if (millis() - printTimepoint > 1000U)
+  {
+    printTimepoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+    {
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+    }
+
+    // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
+
+    // temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+    // temperature compensation
+    float compensationVoltage = averageVoltage / compensationCoefficient;
+
+    // convert voltage value to tds value
+    float KValue = 0.76;
+    tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * KValue;
+    float raw_adc = analogRead(TdsSensorPin);
+    Serial.printf("ADC %.2f   ", raw_adc);
+    Serial.printf("Voltage: %.2fV", averageVoltage);
+    Serial.printf("  TDS Value: %.2f \n", tdsValue);
+  }
 }

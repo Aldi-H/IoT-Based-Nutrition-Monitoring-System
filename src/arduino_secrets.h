82ebdeedb4ceee1d1635f1b2468395d7b1839f19
Aldi-H <aldihimawan88@gmail.com>
2023-04-06 15:40:00 +0700

    chore: add credential

---
+++ src/arduino_secrets.h
@@ -0,0 +1,6 @@
+#define USERNAME "Kuro"
+#define DEVICE_ID "IoT-Nutrition-Monitoring-System"
+#define DEVICE_CREDENTIAL "ftpFsZ!wi4c0AfGL"
+
+#define SSID "POCO X3 NFC"
+#define SSID_PASSWORD "sarasara"

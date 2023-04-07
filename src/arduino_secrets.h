f66474713807b21a7c5e4cf96e0dfe70257c9a00
Aldi-H <aldihimawan88@gmail.com>
2023-04-07 14:58:01 +0700

    chore: adding some credentials

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -2,5 +2,11 @@
 #define DEVICE_ID "IoT-Nutrition-Monitoring-System"
 #define DEVICE_CREDENTIAL "ftpFsZ!wi4c0AfGL"
 
-#define SSID "POCO X3 NFC"
-#define SSID_PASSWORD "sarasara"
+// #define SSID "POCO X3 NFC"
+// #define SSID_PASSWORD "sarasara"
+
+#define SSID "HUAWEI-V945"
+#define SSID_PASSWORD "Ah6Du2JN"
+
+// #define SSID "Kuro"
+// #define SSID_PASSWORD "kuro1905"

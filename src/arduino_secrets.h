dc521e84301613a5c2bf3dcbb32146786c570540
Aldi-H <aldihimawan88@gmail.com>
2023-04-18 09:39:15 +0700

    buid: edited credential

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -1,12 +1,15 @@
 #define USERNAME "Kuro"
-#define DEVICE_ID "IoT-Nutrition-Monitoring-System"
-#define DEVICE_CREDENTIAL "ftpFsZ!wi4c0AfGL"
+#define DEVICE_ID "IoT_Nutrition_Monitoring_System"
+#define DEVICE_CREDENTIAL "DAU#uQX0x0xBwZo+"
 
 // #define SSID "POCO X3 NFC"
 // #define SSID_PASSWORD "sarasara"
 
-#define SSID "HUAWEI-V945"
-#define SSID_PASSWORD "Ah6Du2JN"
+// #define SSID "HUAWEI-V945"
+// #define SSID_PASSWORD "Ah6Du2JN"
+
+#define SSID "Molyono"
+#define SSID_PASSWORD "19012328"
 
 // #define SSID "Kuro"
 // #define SSID_PASSWORD "kuro1905"
